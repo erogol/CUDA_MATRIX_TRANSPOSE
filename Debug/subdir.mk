e5fe7322c5ae9aa0193711a3a17fdaa3fd5f2a2e
erogol <erogol@erogol-K55VJ.(none)>
2013-03-25 03:31:35 +0200

    first commit

---
+++ Debug/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CU_SRCS += \
+../perform_transpose.cu 
+
+CU_DEPS += \
+./perform_transpose.d 
+
+OBJS += \
+./perform_transpose.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cu
+	@echo 'Building file: $<'
+	@echo 'Invoking: NVCC Compiler'
+	nvcc -G -g -O0 -gencode arch=compute_20,code=sm_20 -gencode arch=compute_20,code=sm_21 -odir "" -M -o "$(@:%.o=%.d)" "$<"
+	nvcc --compile -G -O0 -g -gencode arch=compute_20,code=compute_20 -gencode arch=compute_20,code=sm_21  -x cu -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
